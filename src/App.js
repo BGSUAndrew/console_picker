a6f0f9af4d80bf0f83d3673b4949effdd31d6653
Andrew O'Connor <andrew.oconnor.12@gmail.com>
2021-07-05 11:18:48 -0400

    Updated results for PS5, Switch, and Stadia

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { BuzzFeedQuiz } from "react-buzzfeed-quiz";
 import "react-buzzfeed-quiz/lib/styles.css";
 import './App.css';
 import Xbox from "./assets/xbox.jpg";
+import PS5 from "./assets/ps5.jpg"
+import Switch from "./assets/nintendo.png";
+import Stadia from "./assets/stadia.jpg";
 
 const App = () => {
   return (
@@ -120,25 +123,28 @@ const App = () => {
       results={[
         {
           title: "Xbox Series S or X",
-          description: "Your second result description goes here",
+          description: "The Xbox is for you. With two SKUs (Series S: $300. Series X: $500) and plenty of games to play on the Netflix-style service, Game Pass, the Xbox is one of the most flexible gaming options this generation.",
           resultID: 0,
           resultID: 1,
           resultImageSrc: Xbox,
         },
         {
           title: "Playstation 5",
-          description: "Your second result description goes here",
+          description: "The PlayStation 5 is for you. The PS5 has two SKUs this generation. Digital edition at $400 and disc drive included, $500 edition. The PS5 is known for big, movie-like games. If you’re looking for the best single player experience this generation, the PS5 is a great option.",
           resultID: 2,
+          resultImageSrc: PS5,
         },
         {
           title: "Nintendo Switch",
-          description: "Result images and attributions are optional!",
+          description: "The Nintendo Switch is for you. The Switch is technically last generation. Don’t let that stop you. It’s an affordable console at $299 and a $199 SKU is available. Nintendo is famous for family friendly games. If you’re looking for a fun, casual, gaming experience then look no further than the switch. ",
           resultID: 3,
+          resultImageSrc: Switch,
         },
         {
           title: "Google Stadia",
-          description: "Result images and attributions are optional!",
+          description: "Google Stadia is for you. Google Stadia is an all-cloud gaming console. You’re going to need some decent internet speeds and likely unlimited data. However, for just $10 a month (plus the cost of games) you can play on just about any device. Cloud gaming is one of the newest innovations in gaming and getting started is low cost.",
           resultID: 4,
+          resultImageSrc: Stadia,
         },
       ]}
     />
